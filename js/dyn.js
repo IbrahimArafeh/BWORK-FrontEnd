fe23f636e91dad789e9fdbdef06975dc09d41e77
IbrahimArafeh <68711291+IbrahimArafeh@users.noreply.github.com>
2021-11-21 11:57:51 +0200

    Delete Rubbish
    Delete testing html file and css and js that I've added for test

--- js/dyn.js
+++
@@ -1,7 +0,0 @@
-function create() {
-    alert(Hiiiiiiiiiiiii);
-    var h1 = document.createElement('h1');
-    h1.textContent = "New Heading!!!";
-    h1.setAttribute('class', 'note');
-    document.body.appendChild(h1);
-  }
