ccdb65861500cebc296fd9fcb77793b2840f5fc0
IbrahimArafeh <68711291+IbrahimArafeh@users.noreply.github.com>
2021-11-21 11:25:51 +0200

    Force LogOut, Login.js
    change forget password to force logout and add the same function with login
    move js file to login.js

--- js/login.js
+++ js/login.js
@@ -1,33 +1,34 @@
-function pslogincheck() {
-    var userName = document.getElementById("loginUsrename");
-    var password = document.getElementById("loginPassword");
+ // Checking on form fields
+ function getFormElelemets() {
+     var myForm = document.getElementById("formID");
+     for (var i = 0; i < myForm.elements.length; i++) {
+         if (myForm.elements[i].tagName.toLowerCase() == 'input') {
+             var spanEle = document.getElementById(myForm.elements[i].id + "_");
+             if (hasValue(myForm.elements[i]) == false) {
+                 validation(myForm.elements[i], spanEle);
+             } else {
+                 myForm.elements[i].classList.remove("login-input-validation");
+                 myForm.elements[i].classList.add("login-input");
+                 spanEle.style.display = "none";
+             }
+         }
+     }
 
-    if (hasValue(userName) || hasValue(password)) {
-        // alert("there is no value")
+     // Send Value to Back-End
+     //
+ }
 
-    }
-}
+ function hasValue(e) {
+     var value = e.value;
+     if (value == null || value == '' || value == 0) {
+         return false;
+     } else {
+         true;
+     }
+ }
 
-function hasValue(e) {
-    var value = e.value;
-    if (value == null || value == '' || value == 0) {
-        return false;
-    } else {
-        true;
-    }
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    var elements = document.getElementsByTagName("INPUT");
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].oninvalid = function(e) {
-            e.target.setCustomValidity("");
-            if (!e.target.validity.valid) {
-                e.target.setCustomValidity("This field cannot be left blank");
-            }
-        };
-        elements[i].oninput = function(e) {
-            e.target.setCustomValidity("");
-        };
-    }
-})
+ function validation(e, s) {
+     e.classList.remove();
+     e.classList.add("login-input-validation");
+     s.style.display = "block";
+ }
