e6c777242762908b4d6d7cc7ce29d346be931783
IbrahimArafeh <68711291+IbrahimArafeh@users.noreply.github.com>
2021-11-23 10:14:12 +0200

    image situations done
    working on div of image

---
+++ js/psGallery.js
@@ -0,0 +1,27 @@
+var mainImg = document.getElementById("mainImg");
+
+var thmub1 = document.getElementById('thumb1');
+var thumb1Src = document.getElementById('thumb1').src;
+
+var thmub2 = document.getElementById('thumb2');
+var thumb2Src = document.getElementById('thumb2').src;
+
+var thmub3 = document.getElementById('thumb3');
+var thumb3Src = document.getElementById('thumb3').src;
+
+var thmub4 = document.getElementById('thumb4');
+var thumb4Src = document.getElementById('thumb4').src;
+
+thumb1.addEventListener("click", function() {
+    mainImg.src = thumb1Src
+})
+
+thumb2.addEventListener("click", function() {
+    mainImg.src = thumb2Src
+})
+thumb3.addEventListener("click", function() {
+    mainImg.src = thumb3Src
+})
+thumb4.addEventListener("click", function() {
+    mainImg.src = thumb4Src
+})
