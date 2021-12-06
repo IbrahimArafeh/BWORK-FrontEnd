6d6e612ea83aff9af20742d8ae6d93db3b169876
IbrahimArafeh <68711291+IbrahimArafeh@users.noreply.github.com>
2021-12-06 15:28:54 +0200

    image zoom

--- js/psGallery.js
+++ js/psGallery.js
@@ -38,7 +38,6 @@ function searchStringInArray(str, strArray) {
     return -1;
 }
 
-
 //Add Image List 
 function addThumbImage() {
     var picThumbList = document.getElementById('thumbnailList');
@@ -84,4 +83,58 @@ function checkIMG(e) {
     } else {
         return false;
     }
+}
+
+// image zoom
+
+var scale = 1,
+    panning = false,
+    pointX = 0,
+    pointY = 0,
+    start = {
+        x: 0,
+        y: 0
+    },
+    zoom = document.getElementById("imgDiv");
+
+function setTransform() {
+    zoom.style.transform = "translate(" + pointX + "px, " + pointY + "px) scale(" + scale + ")";
+}
+
+zoom.onmousedown = function(e) {
+    e.preventDefault();
+    start = {
+        x: e.clientX - pointX,
+        y: e.clientY - pointY
+    };
+    panning = true;
+}
+
+zoom.onmouseup = function(e) {
+    panning = false;
+}
+
+zoom.onmousemove = function(e) {
+    e.preventDefault();
+    if (!panning) {
+        return;
+    }
+    pointX = (e.clientX - start.x);
+    pointY = (e.clientY - start.y);
+    setTransform();
+}
+
+zoom.onwheel = function(e) {
+    e.preventDefault();
+    xpoint = pointX;
+    xpoint = pointY;
+
+    var xs = (e.clientX - pointX) / scale,
+        ys = (e.clientY - pointY) / scale,
+        delta = (e.wheelDelta ? e.wheelDelta : -e.deltaY);
+    (delta > 0) ? (scale *= 1.2) : (scale /= 1.2);
+    pointX = e.clientX - xs * scale;
+    pointY = e.clientY - ys * scale;
+
+    setTransform();
 }
